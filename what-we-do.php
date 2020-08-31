b5b91bfe83b0d8544a1c92ceeb6b2b8bd3fa3ae3
ssallgood <ssallgood@ssokay.com>
2020-08-31 15:23:12 -0400

    what we do

--- what-we-do.php
+++ what-we-do.php
@@ -13,6 +13,7 @@ Coded by Creative Tim
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
